469b2c48d638d7be4c20a692ce76a0b95b17811a
Captain Anubhav <dennisthecelebutante@gmail.com>
2020-08-06 11:58:15 +0530

    Initial Commit

---
+++ Spot the WASTE lite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spot the WASTE lite
+//
+//  Created by ANUBHAV DAS on 06/08/20.
+//  Copyright © 2020 Captain Anubhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
